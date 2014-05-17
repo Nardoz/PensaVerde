1bd8cfc2da3c739f55d1ae1ad120110cdcbc15ab
Alan Gordon Reid <mail@alanreid.com.ar>
2014-05-17 02:59:38 -0300

    agregué esqueleto de funcionalidad en los controllers

--- controllers.js
+++ controllers.js
@@ -1,40 +1,179 @@
 
+var models = require('./models');
 var controllers = {};
 
 controllers.index = function(req, res) {
-  res.render('index');
+
+  var topTen = models.Tutorial.getTopTen();
+
+  res.render('index', { 
+    topTen: topTen 
+  });
+
 };
 
 controllers.search = function(req, res) {
-  res.render('search');
+
+  var keywords = req.param('keywords', '');
+  var results = [];
+
+  if(keywords) {
+    results = models.Tutorial.findAllByKeywords(keywords);
+  }
+
+  res.render('search', {
+    keywords: keywords,
+    results: results
+  });
 };
 
 controllers.tutorial_add = function(req, res) { // POST
+
+  if(req.body) {
+    if(models.Tutorial.create(req.body)) {
+
+    }
+  }
+
   res.render('tutorial_add');
 };
 
 controllers.tutorial_view = function(req, res) {
-  res.render('tutorial_view');
+
+  var id = req.params.id;
+
+  if(id) {
+    var tutorial = models.Tutorial.findById(id);
+    res.redirect('/tutorial/' + id + '/step/intro');
+  } else {
+    res.send(404, 'Not found');
+  }
+  
 };
 
 controllers.tutorial_edit = function(req, res) { // POST
-  res.render('tutorial_edit');
+  
+  var id = req.params.id;
+
+  if(id) {
+
+    if(req.body) {
+      if(models.TutorialStep.update(req.body)) {
+
+      }
+    }
+
+    var tutorial = models.Tutorial.findById(id);
+
+    res.render('tutorial_edit', {
+      tutorial: tutorial
+    });
+
+  } else {
+    res.send(404, 'Not found');
+  }
+
 };
 
 controllers.tutorial_step_add = function(req, res) { // POST
-  res.render('tutorial_step_add');
+  var id = req.params.id;
+  var step = req.params.step;
+
+  if(id) {
+
+    var tutorial = models.Tutorial.findById(id);
+
+    if(req.body) {
+      if(models.TutorialStep.create(req.body)) {
+
+      }
+    }
+
+    var currentStep = models.TutorialStep.findByTutorial(tutorial, step);
+
+    res.render('tutorial_step_add', {
+      tutorial: tutorial,
+      step: currentStep
+    });
+
+  } else {
+    res.send(404, 'Not found');
+  }
 };
 
 controllers.tutorial_step_view = function(req, res) {
-  res.render('tutorial_step_view');
+
+  var id = req.params.id;
+  var step = req.params.step;
+
+  if(id) {
+    var tutorial = models.Tutorial.findById(id);
+    var currentStep = models.TutorialStep.findByTutorial(tutorial, step);
+
+    res.render('tutorial_step_view', {
+      tutorial: tutorial,
+      step: currentStep
+    });
+  } else {
+    res.send(404, 'Not found');
+  }
+
 };
 
 controllers.tutorial_step_edit = function(req, res) { // POST
-  res.render('tutorial_step_edit');
+
+  var id = req.params.id;
+  var step = req.params.step;
+
+  if(id) {
+
+    var tutorial = models.Tutorial.findById(id);
+    var currentStep = models.TutorialStep.findByTutorial(tutorial, step);
+
+    if(req.body) {
+      if(models.TutorialStep.update(req.body)) {
+
+      }
+    }
+
+    res.render('tutorial_step_edit', {
+      tutorial: tutorial,
+      step: currentStep
+    });
+
+  } else {
+    res.send(404, 'Not found');
+  }
 };
 
 controllers.tutorial_step_photo_add = function(req, res) { // POST
-  res.render('tutorial_step_photo_add');
+  
+  var id = req.params.id;
+  var step = req.params.step;
+
+  if(id) {
+
+    var tutorial = models.Tutorial.findById(id);
+    var currentStep = models.TutorialStep.findByTutorial(tutorial, step);
+
+    if(req.files) {
+
+      fs.readFile(req.files.stepImage.path, function(err, data) {
+        var basePath = __dirname + '/public'; 
+        var path = '/uploads/' + id + '_' + step + '_' + stepImage.name;
+        
+        fs.writeFile(basePath + path, data, function(err) {
+          if(models.TutorialStep.addPhoto(path)) {
+            res.json({ status: 'ok' });
+          }
+        });
+      });
+    }
+
+    res.json({ status: 'failed' });
+  } else {
+    res.json({ status: 'failed' });
+  }
 };
 
 controllers.auth_facebook = function(req, res) { // POST
---
+++ models.js
@@ -0,0 +1,46 @@
+
+var models = {};
+
+
+models.Tutorial = {};
+
+models.Tutorial.create = function(body) {
+
+};
+
+models.Tutorial.update = function(body) {
+
+};
+
+models.Tutorial.findById = function(id) {
+  return {};
+};
+
+models.Tutorial.getTopTen = function() {
+  return {};
+};
+models.Tutorial.findAllByKeywords = function(keywords) {
+  return [];
+};
+
+
+
+models.TutorialStep = {};
+
+models.TutorialStep.create = function(body) {
+  return {};
+};
+
+models.TutorialStep.update = function(body) {
+  return {};
+};
+
+models.TutorialStep.findByTutorial = function(tutorial, step) {
+  return {};
+};
+
+
+
+
+
+module.exports = models;
