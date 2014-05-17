2c57a5f194e0b4e813917090617e7098fa399a25
Alan Gordon Reid <mail@alanreid.com.ar>
2014-05-17 02:05:13 -0300

    Added routes and controllers

--- controllers.js
+++ controllers.js
@@ -1,8 +1,52 @@
 
 var controllers = {};
 
-controllers.home = function(req, res) {
+controllers.index = function(req, res) {
   res.render('index');
+};
+
+controllers.search = function(req, res) {
+  res.render('search');
+};
+
+controllers.tutorial_add = function(req, res) { // POST
+  res.render('tutorial_add');
+};
+
+controllers.tutorial_view = function(req, res) {
+  res.render('tutorial_view');
+};
+
+controllers.tutorial_edit = function(req, res) { // POST
+  res.render('tutorial_edit');
+};
+
+controllers.tutorial_step_add = function(req, res) { // POST
+  res.render('tutorial_step_add');
+};
+
+controllers.tutorial_step_view = function(req, res) {
+  res.render('tutorial_step_view');
+};
+
+controllers.tutorial_step_edit = function(req, res) { // POST
+  res.render('tutorial_step_edit');
+};
+
+controllers.tutorial_step_photo_add = function(req, res) { // POST
+  res.render('tutorial_step_photo_add');
+};
+
+controllers.auth_facebook = function(req, res) { // POST
+  res.render('auth_facebook');
+};
+
+controllers.account_favs = function(req, res) {
+  res.render('account_favs');
+};
+
+controllers.account_profile = function(req, res) {
+  res.render('account_profile');
 };
 
 module.exports = controllers;
--- routes.js
+++ routes.js
@@ -3,6 +3,17 @@ var controllers = require('./controllers');
 
 module.exports = function(app) {
 
-  app.get('/', controllers.home);
+  app.get('/', controllers.index);
+  app.get('/search', controllers.search);
+  app.post('/tutorial', controllers.tutorial_add);
+  app.get('/tutorial/:id', controllers.tutorial_view);
+  app.post('/tutorial/:id', controllers.tutorial_edit);
+  app.post('/tutorial/:id/step', controllers.tutorial_step_add);
+  app.get('/tutorial/:id/step/:step', controllers.tutorial_step_view);
+  app.post('/tutorial/:id/step/:step', controllers.tutorial_step_edit);
+  app.post('/tutorial/:id/step/:step/add-photo', controllers.tutorial_step_photo_add);
+  app.post('/auth/facebook', controllers.auth_facebook);
+  app.get('/account/favs', controllers.account_favs);
+  app.get('/account/profile', controllers.account_profile);
 
 };
